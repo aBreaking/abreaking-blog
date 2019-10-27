6a64ef0a6ee9568f8ca7e621036e61880f9f90cf
aBreaking <abreaking@163.com>
2019-10-27 23:20:42 +0800

    冗余的部分删除

--- src/test/java/com/abreaking/blog/utils/visitor/VisitorTest.java
+++ src/test/java/com/abreaking/blog/utils/visitor/VisitorTest.java
@@ -1,17 +1,11 @@
 package com.abreaking.blog.utils.visitor;
 
-import com.abreaking.blog.service.VisitorService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import javax.annotation.Resource;
-import java.io.File;
-import java.util.List;
-
 /**
  * @{USER}
  * @{DATE}
